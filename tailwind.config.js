fd93ca7ac0090894be382c168d76903c88fb719f
Villani85 <Villani85@users.noreply.github.com>
2025-10-07 10:39:22 +0000

    deploy: 2496096afe705705875d76e6420fa3f50827b881

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      fontFamily: { sans: ["Inter","ui-sans-serif","system-ui"] },
+    },
+  },
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/line-clamp"),
+  ],
+};
